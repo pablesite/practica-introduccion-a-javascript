d5a0a8a72f9bc6c891bb0705d1e314443c289ec7
Pablo Ruiz <pabloruiz@ctnaval.com>
2019-07-02 21:20:54 +0200

    Arranco con el ejercicio

---
+++ Kata1/Kata1.js
@@ -0,0 +1,49 @@
+/*
+# Primera Kata
+## FooBarQuix
+
+Nos dan un número entre el 1 y 100, y tenemos que devolver por orden lo siguiente:
+
+    * Si el número es divisible por 3, escribiremos “Foo” en lugar del número
+* Si el número es divisible por 5, añadimos “Bar”
+* Si el número es divisible por 7, añadimos “Quix”.
+* Por cada dígito 3,5 o 7, añadiremos “Foo”, “Bar”, “Quix” respectivamente y en orden de aparición.
+*/  
+    
+
+function foobarquix (num) {
+    switch (num) {
+        case valor1:
+            //Sentencias ejecutadas cuando el resultado de expresion coincide con valor1
+            [break;]
+        case valor2:
+            //Sentencias ejecutadas cuando el resultado de expresion coincide con valor2
+            [break;]
+        ...
+        case valorN:
+            //Sentencias ejecutadas cuando el resultado de expresion coincide con valorN
+            [break;]
+        default:
+            //Sentencias_def ejecutadas cuando no ocurre una coincidencia con los anteriores casos
+            [break;]
+    }
+
+}
+
+
+
+    switch valor {
+        (es 3){
+            escribo foo
+        }
+        (es 5){
+            escribo bar
+        }
+        (es 7){
+            escribo Quix
+        }
+
+    }
+}
+
+/* Ejecución de prueba */
