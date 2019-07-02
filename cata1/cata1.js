d406bd1e572699865867422230f099f3054a50d3
Pablo Ruiz <pabloruiz@ctnaval.com>
2019-07-02 20:52:56 +0200

    Commit inicial. Creo carpeta cata1 y un index.js

---
+++ cata1/cata1.js
@@ -0,0 +1,24 @@
+
+
+foobarquix {
+
+    if,
+    switch
+    operadores
+
+
+    switch valor {
+        (es 3){
+            escribo foo
+        }
+        (es 5){
+            escribo bar
+        }
+        (es 7){
+            escribo Quix
+        }
+
+    }
+}
+
+/* Ejecución de prueba */
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Fichero leido desde consola");
